a6b7af34dd794835c05243d75e026eb93a0d33e3
Oleh Luzhniak <oleg.luzhnyak@gmail.com>
2023-06-28 18:53:55 +0300

    task1-3

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,19 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const listEl = document.querySelector("#ingredients");
+
+const itemsEl = ingredients.map((ingredient) => {
+  const ingredientEl = document.createElement("li");
+  ingredientEl.classList.add("item");
+  ingredientEl.textContent = ingredient;
+  return ingredientEl;
+});
+
+listEl.append(...itemsEl);
